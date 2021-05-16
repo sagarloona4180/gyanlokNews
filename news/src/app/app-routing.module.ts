1be481fe1df91bc3c1021323a81cce214d98b388
Sagar Kumar <sagarkum@cisco.com>
2021-05-16 11:45:41 +0530

    fixes

--- news/src/app/app-routing.module.ts
+++ news/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NewsContentComponent } from './components/new-post/news-content/news-content.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+
+  {
+    path:"",
+    pathMatch:'full',
+    redirectTo :'dashboard'
+  },
+
+  {
+    path: "dashboard",
+    component:DashboardComponent
+  },
+  {
+    path: "news",
+    component: NewsContentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
