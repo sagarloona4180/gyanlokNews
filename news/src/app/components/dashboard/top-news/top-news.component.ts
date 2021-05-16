f1dde97c1d9743354812f3e2892ee5459bd1bb41
Sagar Kumar <sagarkum@cisco.com>
2021-05-16 23:09:40 +0530

    new fixes

--- news/src/app/components/dashboard/top-news/top-news.component.ts
+++ news/src/app/components/dashboard/top-news/top-news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-news',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNewsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Route:Router) { }
 
   ngOnInit(): void {
+  }
+
+  gotoNews(){
+
+    this.Route.navigate(['./news']);
   }
 
 }
