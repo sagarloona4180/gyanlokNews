63c34041989359a55b18d70f6755961ae4ae376f
Sagar Kumar <sagarkum@cisco.com>
2021-05-25 01:23:57 +0530

    added text

--- news/src/app/components/email-us/email-us.component.ts
+++ news/src/app/components/email-us/email-us.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { URLs } from 'src/app/common/constant/constant';
 
 @Component({
   selector: 'app-email-us',
@@ -6,10 +10,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-us.component.scss']
 })
 export class EmailUsComponent implements OnInit {
+  form: FormGroup = new FormGroup({
 
-  constructor() { }
+    Name: new FormControl('', [Validators.required]),
+
+    Contact: new FormControl('', [Validators.required,  Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$"),Validators.maxLength(10)]),
+
+    Email: new FormControl('', [Validators.required]),
+
+    Message: new FormControl('', [Validators.required])
+
+    
+
+  });
+  url =URLs.getAPIUrl()+'createMail.php';
+  error =false;
+  sucess: boolean = false;
+  constructor(private http:HttpClient,private route:Router) { }
 
   ngOnInit(): void {
   }
+
+
+
+  validateEmail(email:string) {
+    const reg = /^(([^<>()\[\]\\.,;:\s@']+(\.[^<>()\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return reg.test(String(email).toLowerCase());
+  }
+
+  send(){
+    const obj:any =
+      this.form.value;
+  this.sucess = true;
+    
+   if(this.validateEmail(obj.Email)){
+    this.http.post(this.url,obj ).subscribe(x => {
+            this.sucess = false;
+            this.form.reset();
+    },err=>{
+       this.sucess = false;
+    });
+  }else {
+   this.sucess = false;
+   this.error = true;
+  }
+
+}
 
 }
--- news/src/app/components/subscribe-us/subscribe-us.component.ts
+++ news/src/app/components/subscribe-us/subscribe-us.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { URLs } from 'src/app/common/constant/constant';
 
 @Component({
   selector: 'app-subscribe-us',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscribe-us.component.scss']
 })
 export class SubscribeUsComponent implements OnInit {
-
-  constructor() { }
+  url =URLs.getAPIUrl()+'createSub.php';
+  email='';
+  sucess =false;
+  error: boolean = false;
+  loader: boolean = false;
+  constructor(private http:HttpClient,private route:Router) { }
 
   ngOnInit(): void {
   }
+  onKey(e:any){
+    this.email= e.target.value;
+    this.error =false
+  }
 
+  subscribe(){
+    const obj:any = {
+      Email:this.email
+    };
+    this.loader = true;
+   if(this.validateEmail(obj.Email)){
+    this.http.post(this.url,obj ).subscribe(x => {
+            this.sucess = true;
+            this.loader = false;
+    },err=>{
+      this.loader = false;
+    });
+  }else {
+   this.error = true;
+   this.loader = false;
+  }
+
+}
+
+  close(){
+    this.route.navigate(['./dashboard']);
+  }
+
+  
+  validateEmail(email:string) {
+    const reg = /^(([^<>()\[\]\\.,;:\s@']+(\.[^<>()\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return reg.test(String(email).toLowerCase());
+  }
 }
